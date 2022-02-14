54a0a538bce145b7e1c03249dcc2651a1f64313d
Ramom Oliveira <ra-mom2000@hotmail.com>
2022-02-13 21:40:38 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+
+// eslint-disable-next-line func-names
+const MyApp = function ({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line react/function-component-definition
+export default function Home() {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+    </div>
+  );
+}
