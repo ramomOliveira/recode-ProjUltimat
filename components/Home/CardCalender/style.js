4e9ff79ed505e78940fe6221586844cd046f4e8c
Ramom Oliveira <ra-mom2000@hotmail.com>
2022-03-22 13:03:36 -0300

    fit mobile home page

--- components/Home/CardCalender/style.js
+++ components/Home/CardCalender/style.js
@@ -5,16 +5,19 @@ export const Container = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  width: 200px;
+  width: 100%;
   height: 350px;
+
+  @media (min-width: 768px) {
+    width: 200px;
+  }
 `;
 
 export const Wrapper = styled.div`
   background-color: rgba(0, 0, 0, 0.7);
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  gap: 60px;
   width: 100%;
   height: 100%;
   color: var(--white);
@@ -23,17 +26,40 @@ export const Wrapper = styled.div`
 
   
   > h1 {
-    font-size: 32px;
+    font-size: 42px;
     margin-bottom: 20px;
+    margin-top: 20px;
     text-align: center;
   }
   > p {
-    font-size: 23px;
+    font-size: 30px;
     line-height: 24px;
     margin-bottom: 16px;
   }
   > h2 {
-    font-size: 16px;
+    font-size: 25px;
     margin-bottom: 16px;
+  }
+
+  @media (min-width: 768px) {
+    align-items: center;
+    justify-content: center;
+    gap: 0px;
+
+    > h1 {
+    font-size: 32px;
+    margin-bottom: 20px;
+    margin-top: 0px;
+    text-align: center;
+    }
+    > p {
+      font-size: 23px;
+      line-height: 24px;
+      margin-bottom: 16px;
+    }
+    > h2 {
+      font-size: 16px;
+      margin-bottom: 16px;
+    }
   }
 `;
--- components/Home/RangeCalender/style.js
+++ components/Home/RangeCalender/style.js
@@ -6,6 +6,7 @@ export const Container = styled.div`
   margin-top: 50px;
   margin-bottom: 50px;
   align-items: center;
+  
 
   @media (min-width: 768px) {
     grid-template-columns: 30% 1fr;
@@ -16,17 +17,21 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
-  align-items: flex-start;
+  align-items: center;
 
   > h1 {
     font-size: 40px;
+  }
+
+  @media (min-width: 768px) {
+    align-items: flex-start;
   }
 `;
 
 export const WrapperCards = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
-
+  gap: 15px;
   @media (min-width: 768px) {
     grid-template-columns: repeat(3, 1fr);
   }
